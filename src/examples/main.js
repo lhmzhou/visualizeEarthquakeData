7ad48a0b3c27f829a284fabd61f494a04fd0b4a3
lhmzhou <lhmzhou@gmail.com>
2018-01-09 00:26:08 -0600

    Create main.js

---
+++ src/examples/main.js
@@ -0,0 +1,10 @@
+define(function(require) {
+  var d3 = require('ds');
+  var topojson = require('topojson');
+  var E-data Visual = require('datamaps');
+  console.log('woah');
+console.log(d3, topojson);
+    // Begin drawing for SVG map
+   var map = new Datamap({element: document.getElementById('container')});
+
+});
